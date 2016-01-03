2cdb44797c385656da3cf1e8d2e5877800d5368b
eagle14 <aghayeeagle@gmail.com>
2016-01-03 10:09:34 +0330

    Update banhammer.lua

--- plugins/banhammer.lua
+++ plugins/banhammer.lua
@@ -1,216 +1,75 @@
-local function is_user_whitelisted(id)
-  local hash = 'whitelist:user#id'..id
-  local white = redis:get(hash) or false
-  return white
-end
+/*
+ * Copyright 2009-2011  Keith Rusler <xzekecomax@gmail.com>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy
+ * defined in Section 14 of version 3 of the license.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ */
 
-local function is_chat_whitelisted(id)
-  local hash = 'whitelist:chat#id'..id
-  local white = redis:get(hash) or false
-  return white
-end
+#include "kickreply.hpp"
+#include "private/kickreply_p.hpp"
+using namespace Aki;
+using namespace Irc;
 
-local function kick_user(user_id, chat_id)
-  local chat = 'chat#id'..chat_id
-  local user = 'user#id'..user_id
+KickReply::KickReply()
+    : Aki::Irc::Reply(),
+    _d(new Aki::Irc::KickReplyPrivate)
+{
+}
 
-  if user_id == tostring(our_id) then
-    send_msg(chat, "I won't kick myself!", ok_cb,  true)
-  else
-    chat_del_user(chat, user, ok_cb, true)
-  end
-end
+KickReply::KickReply(const Aki::Irc::ReplyInfo& replyInfo)
+    : Aki::Irc::Reply(replyInfo),
+    _d(new Aki::Irc::KickReplyPrivate)
+{
+    _d->message = reply().params().value(2, QString());
+    _d->channel = reply().params().at(0);
+    _d->nick = reply().params().at(1);
+}
 
-local function ban_user(user_id, chat_id)
-  -- Save to redis
-  local hash =  'banned:'..chat_id..':'..user_id
-  redis:set(hash, true)
-  -- Kick from chat
-  kick_user(user_id, chat_id)
-end
+KickReply::KickReply(const Aki::Irc::KickReply& other)
+    : Aki::Irc::Reply(other),
+    _d(other._d)
+{
+}
 
-local function is_banned(user_id, chat_id)
-  local hash =  'banned:'..chat_id..':'..user_id
-  local banned = redis:get(hash)
-  return banned or false
-end
+KickReply::~KickReply()
+{
+}
 
-local function pre_process(msg)
+Aki::Irc::KickReply&
+KickReply::operator=(const Aki::Irc::KickReply& other)
+{
+    Aki::Irc::Reply::operator=(other);
+    _d = other._d;
+    return *this;
+}
 
-  -- SERVICE MESSAGE
-  if msg.action and msg.action.type then
-    local action = msg.action.type
-    -- Check if banned user joins chat
-    if action == 'chat_add_user' or action == 'chat_add_user_link' then
-      local user_id
-      if msg.action.link_issuer then
-        user_id = msg.from.id
-      else
-	      user_id = msg.action.user.id
-      end
-      print('Checking invited user '..user_id)
-      local banned = is_banned(user_id, msg.to.id)
-      if banned then
-        print('User is banned!')
-        kick_user(user_id, msg.to.id)
-      end
-    end
-    -- No further checks
-    return msg
-  end
+QString
+KickReply::channel() const
+{
+    return _d->channel;
+}
 
-  -- BANNED USER TALKING
-  if msg.to.type == 'chat' then
-    local user_id = msg.from.id
-    local chat_id = msg.to.id
-    local banned = is_banned(user_id, chat_id)
-    if banned then
-      print('Banned user talking!')
-      ban_user(user_id, chat_id)
-      msg.text = ''
-    end
-  end
+QString
+KickReply::message() const
+{
+    return _d->message;
+}
 
-  -- WHITELIST
-  local hash = 'whitelist:enabled'
-  local whitelist = redis:get(hash)
-  local issudo = is_sudo(msg)
-
-  -- Allow all sudo users even if whitelist is allowed
-  if whitelist and not issudo then
-    print('Whitelist enabled and not sudo')
-    -- Check if user or chat is whitelisted
-    local allowed = is_user_whitelisted(msg.from.id)
-
-    if not allowed then
-      print('User '..msg.from.id..' not whitelisted')
-      if msg.to.type == 'chat' then
-        allowed = is_chat_whitelisted(msg.to.id)
-        if not allowed then
-          print ('Chat '..msg.to.id..' not whitelisted')
-        else
-          print ('Chat '..msg.to.id..' whitelisted :)')
-        end
-      end
-    else
-      print('User '..msg.from.id..' allowed :)')
-    end
-
-    if not allowed then
-      msg.text = ''
-    end
-
-  else
-    print('Whitelist not enabled or is sudo')
-  end
-
-  return msg
-end
-
-local function run(msg, matches)
-
-  -- Silent ignore
-  if not is_sudo(msg) then
-    return nil
-  end
-
-  if matches[1] == 'ban' then
-    local user_id = matches[3]
-    local chat_id = msg.to.id
-
-    if msg.to.type == 'chat' then
-      if matches[2] == 'user' then
-        ban_user(user_id, chat_id)
-        return 'User '..user_id..' banned'
-      end
-      if matches[2] == 'delete' then
-        local hash =  'banned:'..chat_id..':'..user_id
-        redis:del(hash)
-        return 'User '..user_id..' unbanned'
-      end
-    else
-      return 'This isn\'t a chat group'
-    end
-  end
-
-  if matches[1] == 'kick' then
-    if msg.to.type == 'chat' then
-      kick_user(matches[2], msg.to.id)
-    else
-      return 'This isn\'t a chat group'
-    end
-  end
-
-  if matches[1] == 'whitelist' then
-    if matches[2] == 'enable' then
-      local hash = 'whitelist:enabled'
-      redis:set(hash, true)
-      return 'Enabled whitelist'
-    end
-
-    if matches[2] == 'disable' then
-      local hash = 'whitelist:enabled'
-      redis:del(hash)
-      return 'Disabled whitelist'
-    end
-
-    if matches[2] == 'user' then
-      local hash = 'whitelist:user#id'..matches[3]
-      redis:set(hash, true)
-      return 'User '..matches[3]..' whitelisted'
-    end
-
-    if matches[2] == 'chat' then
-      if msg.to.type ~= 'chat' then
-        return 'This isn\'t a chat group'
-      end
-      local hash = 'whitelist:chat#id'..msg.to.id
-      redis:set(hash, true)
-      return 'Chat '..msg.to.id..' whitelisted'
-    end
-
-    if matches[2] == 'delete' and matches[3] == 'user' then
-      local hash = 'whitelist:user#id'..matches[4]
-      redis:del(hash)
-      return 'User '..matches[4]..' removed from whitelist'
-    end
-
-    if matches[2] == 'delete' and matches[3] == 'chat' then
-      if msg.to.type ~= 'chat' then
-        return 'This isn\'t a chat group'
-      end
-      local hash = 'whitelist:chat#id'..msg.to.id
-      redis:del(hash)
-      return 'Chat '..msg.to.id..' removed from whitelist'
-    end
-
-  end
-end
-
-return {
-  description = "Plugin to manage bans, kicks and white/black lists.",
-  usage = {
-    "!whitelist <enable>/<disable>: Enable or disable whitelist mode",
-    "!whitelist user <user_id>: Allow user to use the bot when whitelist mode is enabled",
-    "!whitelist chat: Allow everybody on current chat to use the bot when whitelist mode is enabled",
-    "!whitelist delete user <user_id>: Remove user from whitelist",
-    "!whitelist delete chat: Remove chat from whitelist",
-    "!ban user <user_id>: Kick user from chat and kicks it if joins chat again",
-    "!ban delete <user_id>: Unban user",
-    "!kick <user_id> Kick user from chat group"
-  },
-  patterns = {
-    "^!(whitelist) (enable)$",
-    "^!(whitelist) (disable)$",
-    "^!(whitelist) (user) (%d+)$",
-    "^!(whitelist) (chat)$",
-    "^!(whitelist) (delete) (user) (%d+)$",
-    "^!(whitelist) (delete) (chat)$",
-    "^!(ban) (user) (%d+)$",
-    "^!(ban) (delete) (%d+)$",
-    "^!(kick) (%d+)$",
-    "^!!tgservice (.+)$",
-  },
-  run = run,
-  pre_process = pre_process
+QString
+KickReply::nick() const
+{
+    return _d->nick;
 }
